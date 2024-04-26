f9049ca82c77ab0cf82d19c0aab19aa63def344e
Guejia21 <jonathanguejia@unicauca.edu.co>
2024-04-26 15:46:35 -0500

    Nuevos test de Sign Up
    Errores con el mensaje "Por favor ingrese toda la información solicitada"

--- placita-app/src/__test__/SignUp.test.tsx
+++ placita-app/src/__test__/SignUp.test.tsx
@@ -5,18 +5,18 @@
  */
 
 // Importa las funciones necesarias de Testing Library y React
-import { render,fireEvent } from '@testing-library/react';
+import { render,fireEvent,waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
  // Importa las extensiones de expect de Jest
 
 // Importa el componente Login que deseas probar
-import SignUp from '../components/SignUp/SignUp.tsx';
+import SignUp from '../components/SignUp/SignUp';
 import { BrowserRouter } from 'react-router-dom';
 
 document.body.innerHTML = '<div id="popup-root"></div>';
 
-// Prueba para verificar si el componente SignUp se renderiza correctamente
-test('renders SignUp component', () => { //El error se encuentra en SignUp.css al hacer #input
+
+  test('renders SignUp component', () => { 
     // Renderiza el componente Login
     const { getByText, getByLabelText } = render(
       <BrowserRouter>
@@ -44,15 +44,6 @@ test('renders SignUp component', () => { //El error se encuentra en SignUp.css a
     expect(userAdressInput).toBeInTheDocument();
     expect(submitButton).toBeInTheDocument();
   });
-  /*test('Guild code number wrong',()=>{
-    const { getByText, getByLabelText } = render(
-        <BrowserRouter>
-          <SignUp />
-        </BrowserRouter>  
-      );
-  }
-
-  );*/
   test('enables Agrocode input when "Sí" is selected', () => {
     const { getByLabelText } = render(
     <BrowserRouter>
@@ -68,3 +59,80 @@ test('renders SignUp component', () => { //El error se encuentra en SignUp.css a
     const agrocodeInput = getByLabelText('Codigo Agrocauca');
     expect(agrocodeInput).toBeEnabled();
   });
+  test('Guild code number wrong',async()=>{
+    const { getByText, getByLabelText } = render(
+        <BrowserRouter>
+          <SignUp />
+        </BrowserRouter>  
+      );
+      const userNameInput = getByLabelText('Nombre');
+      const userLastNameInput = getByLabelText('Apellido');
+      const userPasswordInput = getByLabelText('Contraseña');
+      const userIdInput = getByLabelText('Cédula');
+      const userTelInput = getByLabelText('Teléfono');
+      const userAdressInput = getByLabelText('Dirección');
+      const submitButton = getByText('Registrarse');
+      
+      //Insert all the required values
+      fireEvent.change(userNameInput,{target:{value:'Jorge'}});
+      fireEvent.change(userLastNameInput,{target:{value:'Perez'}});
+      fireEvent.change(userPasswordInput,{target:{value:'papitas'}});
+      fireEvent.change(userIdInput,{target:{value:'1058666148'}});
+      fireEvent.change(userTelInput,{target:{value:'3158479160'}});
+      fireEvent.change(userAdressInput,{target:{value:'Santa Elena'}});
+      const yesRadioButton = getByLabelText('Si');
+      
+      //Click on "Si" to enable Agrocode´s input
+      fireEvent.click(yesRadioButton);
+
+      const agrocodeInput = getByLabelText('Codigo Agrocauca');
+      fireEvent.change(agrocodeInput,{target:{value:'0'}});
+
+      fireEvent.click(submitButton);
+      await waitFor(() => {
+        const errorMessage = getByText('Codigo de gremio incorrecto');
+        expect(errorMessage).toBeInTheDocument();
+      }); 
+  }
+
+  );
+  test('Sign in with Agrocode but with empty fields (information)',async()=>{
+    const { getByText, getByLabelText } = render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>  
+    );
+    const yesRadioButton = getByLabelText('Si');
+    const submitButton = getByText('Registrarse');  
+    //Click on "Si" to enable Agrocode´s input
+    fireEvent.click(yesRadioButton);
+
+    const agrocodeInput = getByLabelText('Codigo Agrocauca');
+    //Enter a value in the code
+    fireEvent.change(agrocodeInput,{target:{value:'0'}});
+    //Click on "Registrarse"
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const errorMessage = getByText('Por favor ingrese toda la información solicitada');
+      expect(errorMessage).toBeInTheDocument();
+    }); 
+  }
+  );
+  test('Sign in with empty fields (information)',async()=>{
+    const { getByText } = render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>  
+    );
+    const submitButton = getByText('Registrarse');  
+    //Click on "Registrarse"
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const errorMessage = getByText('Por favor ingrese toda la información solicitada');
+      expect(errorMessage).toBeInTheDocument();
+    }); 
+  }
+  );
+  
