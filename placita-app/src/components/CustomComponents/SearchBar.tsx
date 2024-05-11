bd3760591c24bbcf5dc84b3d708131ab9f115c2e
Whooas <fredyanaya@unicauca.edu.co>
2024-05-11 13:30:00 -0500

    little changes to searchbar

--- placita-app/src/components/CustomComponents/SearchBar.tsx
+++ placita-app/src/components/CustomComponents/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import MaterialSymbolsIcon from '../Icons/MaterialSymbolsIcon';
 
 interface SearchBarProps {
 
@@ -7,11 +8,24 @@ interface SearchBarProps {
 const SearchBar: FC<SearchBarProps> = () => {
     return <search>
         <input>
-            
-        </input>
-        <select>
 
-        </select>
+        </input>
+        <button>
+            <MaterialSymbolsIcon name="search" opsz="48" weight='900' color='black' />
+        </button>
+        <span className="vertical-line" />
+        <span>
+            Ordenar por:
+
+            <select>
+                <option>
+                    precio
+                </option>
+                <option>
+                    nombre
+                </option>
+            </select>
+        </span>
 
     </search>
 
